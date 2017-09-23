dd7c75bdabf8f8ef774502538804b54b5c77123f
Daniel Oliveira <daniel.oliveira079@gmail.com>
2017-09-23 15:46:51 +0200

    add body and footer

---
+++ body/server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var morgan = require('morgan')
+
+var app = express()
+
+const port = process.env.PORT || 5001
+
+app.use(morgan('combined'))
+app.use('/static', express.static('public'))
+
+app.get('/', function (req, res) {
+  res.status(200).send('Hello, I am body')
+})
+
+app.listen(port, function () {
+  console.log('Server listening on port ' + port + '!')
+})
